ea63e6805578f7b0835a3c7b5700dc14518bf7e3
MaxMEllon <mozi_kke@excite.co.jp>
2014-11-25 14:38:52 +0900

    余計なもの削除 httpsに変更

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ cd
 for p in .dotfiles/.zsh.d .dotfiles/.zsh.d/package/zsh.d
 do
   repo `echo $p | sed -e "s/.dotfiles\///g"`
-  git clone git@github.com:${GITHU_USER_NAME}/${repo}.git ${p}
+  git clone https://github.com/${GITHU_USER_NAME}/${repo}.git ${p}
 done
 ln -s .dotfiles/.zsh.d/.zshrc
 if [ ! -e .zshenv ]; then
