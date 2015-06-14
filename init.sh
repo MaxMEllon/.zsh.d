256f4a09647cdb3886a98ed06a66fd12d7ee003a
MaxMEllon <mozi_kke@excite.co.jp>
2015-06-15 06:54:54 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -4,15 +4,8 @@ mkdir ip
 curl "ifconfig.me/ip" > ~/.dotfiles/.zsh.d/ip/ip_data.dat
 curl "ifconfig.me/host" >> ~/.dotfiles/.zsh.d/ip/ip_data.dat
 
+git submodule update --init
 cd
-github_user_name=MaxMEllon
-local_path=(.dotfiles/.zsh.d .dotfiles/.zsh.d/package/zsh.d)
-repo=(.zsh.d zsh.d)
-count=(0 1)
-for k in count
-do
-  git clone https://github.com/${github_user_name}/${repo[$k]}.git $local_path[$k]
-done
 ln -s .dotfiles/.zsh.d/.zshrc
 if [ ! -e .zshenv ]; then
   touch .zshenv
