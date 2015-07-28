b3805f5092a10001a5a0a8fd68c66a2535dd3740
MaxMEllon <mozi_kke@excite.co.jp>
2015-07-28 09:49:20 +0900

    fix: script

--- init.sh
+++ init.sh
@@ -3,12 +3,11 @@ ln -s .dotfiles/.zsh.d/.zshrc ~/
 if [ ! -e ~/.zshenv ]; then
   touch ~/.zshenv
 fi
+git submodule update --init
 echo "source ~/.dotfiles/.zsh.d/package/zsh.d/zshenv" >> ~/.zshenv
-ZSHRC_PATH=~/.dotfiles/.zsh.d
+ZSHRC_PATH=.dotfiles/.zsh.d
 mkdir -p $ZSHRC_PATH/ip
 curl "ifconfig.me/ip" > $ZSHRC_PATH/ip/ip_data.dat
 curl "ifconfig.me/host" >> $ZSHRC_PATH/ip/ip_data.dat
-IP=`head -1 ~/.dotfiles/.zsh.d/ip/ip_data.dat | grep 1`
-cd $ZSHRC_PATH
-git submodule update --init
+IP=`head -1 ${ZSHRC_PATH}/ip/ip_data.dat | grep 1`
 
